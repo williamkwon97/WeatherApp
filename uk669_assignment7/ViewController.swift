dfaaf89cd3bbe3672072f5203e6e6bb2c5c48e3f
William Kwon <williamkwon97@gmail.com>
2020-04-24 17:33:14 -0500

    final

--- uk669_assignment7/ViewController.swift
+++ uk669_assignment7/ViewController.swift
@@ -7,144 +7,163 @@
 //
 
 import UIKit
+import Foundation
 
 
+class ViewController: UIViewController, UISearchBarDelegate, UITextFieldDelegate {
+    @IBOutlet weak var TempLabel: UILabel!
+      @IBOutlet weak var weatherImageView: UIImageView!
+      @IBOutlet weak var cloudcoverLabel: UILabel!
+      @IBOutlet weak var HumidityLabel: UILabel!
+      @IBOutlet weak var PressureLabel: UILabel!
+      @IBOutlet weak var PrecipitationLabel: UILabel!
+      @IBOutlet weak var windLabel: UILabel!
 
-class ViewController: UIViewController, UISearchBarDelegate {
-
+    @IBOutlet weak var errormsgLabel: UILabel!
     @IBOutlet weak var CitySearchBar: UITextField!
     @IBOutlet weak var StateSearchBar: UITextField!
-    @IBOutlet weak var CheckConditionButton: UIButton!
-   
-    @IBOutlet weak var TempLabel: UILabel!
-    @IBOutlet weak var weatherImageView: UIImageView!
-    @IBOutlet weak var cloudcoverLabel: UILabel!
-    @IBOutlet weak var HumidityLabel: UILabel!
-    @IBOutlet weak var PressureLabel: UILabel!
-    @IBOutlet weak var PrecipitationLabel: UILabel!
-    @IBOutlet weak var windLabel: UILabel!
-    
-    func getWeather(loc: String) {
-        super.viewDidLoad()
-        guard let url = URL(string:"https://api.worldweatheronline.com/premium/v1/weather.ashx?key=71f187bbe97b495988c165458182610&format=json") else {return}
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-                   if let data = data, error == nil {
-                       do {
-                           guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String : Any] else {return}
-                           guard let weatherDetails = json["weather"] as? [[String : Any]], let weatherMain = json["main"] as? [String : Any], let weatherWind = json["wind"] as? [String : Any], let weatherSystem = json["sys"] as? [String : Any]else {return}
-                           let temp = Int(weatherMain["temp"] as? Double ?? 0)
-                      
-                           let humidity = Int(weatherMain["humidity"] as? Double ?? 0)
-                           let windSpeed = Int(weatherWind["speed"] as? Double ?? 0)
-                           let windDirection = Int(weatherWind["deg"] as? Double ?? 0)
-                          
-                           let city = (json["name"] ?? "...")
-                           let country = (weatherSystem["country"] ?? "...")
-                           DispatchQueue.main.async {
-                               self.setWeather(weather: weatherDetails.first?["main"] as? String,  temp: temp, humidity: humidity, windSpeed: windSpeed, windDirection: windDirection, city: city, country: country)
-                           }
-                       } catch {
-                           print("We had an error retriving the weather...")
-                       }
-                   }
-               }
-               task.resume()
+    @IBAction func enterInput(_ sender: UIButton) {
+        func displayMyAlertMessage(userMessage:String){
+
+            let myAlert = UIAlertController(title: "Invalid Input", message: userMessage, preferredStyle: UIAlertController.Style.alert)
+            let okAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+           myAlert.addAction(okAction)
+            self.present(myAlert, animated: true, completion: nil)
+        }
+         if (CitySearchBar.text == "") && (StateSearchBar.text == "") {
+            CitySearchBar.layer.borderColor = UIColor.red.cgColor
+            StateSearchBar.layer.borderColor = UIColor.red.cgColor
+            displayMyAlertMessage(userMessage: "All fields are required")
+            return
+        }
+        
+            CitySearchBar.text = CitySearchBar.text?.replacingOccurrences(of: " ", with: "_")
+        StateSearchBar.text = StateSearchBar.text?.replacingOccurrences(of: " ", with: "_")
+        let city = CitySearchBar.text!
+        let state = StateSearchBar.text!
+       
+        let loc = "\(city),\(state)"
+        
+     
+           let location = loc
+          
+           guard let url = URL(string:"https://api.worldweatheronline.com/premium/v1/weather.ashx?key=c1000961f636421bb5e52941202104&format=json&q=\(location)") else {return}
            
+           let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+           
+
+          
+                      if let data = data, error == nil {
+                          do {
+                            let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as! NSDictionary
+                            print(json)
+                            
+                            if (json.value(forKeyPath: "data.error.msg") != nil){
+                                                        DispatchQueue.main.async {
+                                            
+                                                                        self.errormeesage()
+                                                            
+                                                        }
+                                return print("ther was error")
+                                                        }
+                           
+                       
+                            let tempF = json.value(forKeyPath: "data.current_condition.temp_F") as Any
+                            let tempC = json.value(forKeyPath: "data.current_condition.temp_C") as Any
+                            
+                            let humidity = (json.value(forKeyPath: "data.current_condition.humidity") as Any)
+                          
+                            let windSpeed = (json.value(forKeyPath: "data.current_condition.windspeedMiles") as Any)
+                        
+                            let windDirection = (json.value(forKeyPath: "data.current_condition.winddir16Point") as Any )
+                          
+                            let precipInches =
+                                (json.value(forKeyPath: "data.current_condition.precipMM") as Any)
+                        
+                            let pressureInches =
+                                                 (json.value(forKeyPath: "data.current_condition.pressure") as  Any)
+                                                 
+                            let cloudcover =
+                                (json.value(forKeyPath: "data.current_condition.cloudcover") as Any)
+                            let winddirDegree =
+                            (json.value(forKeyPath: "data.current_condition.winddirDegree") as Any)
+                            let windspeedKmph =
+                            (json.value(forKeyPath: "data.current_condition.windspeedKmph") as Any)
+
+                            let weatherIconUrl = (json.value(forKeyPath: "data.current_condition.weatherIconUrl.value") as Any)
+        
+                           
+                            DispatchQueue.main.async {
+                                self.setWeather(tempF: tempF as! NSArray, tempC:tempC as! NSArray,humidity:humidity as! NSArray,windSpeed:windSpeed as! NSArray,windDirection:windDirection as! NSArray, precipInches: precipInches as! NSArray, pressureInches:pressureInches as! NSArray,cloudcover:cloudcover as! NSArray, weatherIconUrl:weatherIconUrl as! NSArray,winddirDegree:winddirDegree as! NSArray,windspeedKmph:windspeedKmph as! NSArray)
+                              }
+                          } catch {
+                              print("We had an error retriving the weather...")
+                          }
+                      }
+                  }
+                  task.resume()
+        
     }
-    
     
           override func viewDidLoad() {
               super.viewDidLoad()
               // Do any additional setup after loading the view, typically from a nib.
-              getWeather(loc: "Poway")
+            
+
           }
           //MARK SET WEATHER
-          func setWeather(weather: String?, description: String?, temp: Int, humidity: Int, windSpeed: Int, windDirection: Int, low: Int, high: Int, city: Any, country: Any) {
-              TempLabel.text = "\(temp)˚"
-              HumidityLabel.text = "Humidity: \(humidity)%"
-              windLabel.text = "Wind: \(windSpeed) MPH \(compassDirection(heading: Double(windDirection)))"
-    
-              place.text = "\(city), \(country)"
-              let clear = UIColor(red: 0.94901960784, green: 0.94901960784, blue: 0.16470588235, alpha: 1)
-              let cloud =  UIColor(red: 0.68235294117, green: 0.83921568627, blue: 0.94509803921, alpha: 1)
-              let snow = UIColor(red: 0.52156862745, green: 0.5725490196, blue: 0.61960784313, alpha: 1)
-              let fog = UIColor(red: 0.94901960784, green: 0.95294117647, blue: 0.95686274509, alpha: 1)
-              let rain = UIColor(red: 0, green: 0.47843137254, blue: 1, alpha: 1)
-              let thunder = UIColor(red: 0.95294117647, green: 0.61176470588, blue: 0.07058823529  , alpha: 1)
-              switch weather {
-              case "Clear":
-                  weatherImageView.image = UIImage(named: "Sunny")
-                  background.backgroundColor = clear
-                  backgroundtwo.backgroundColor = clear
-                  scroll.backgroundColor = clear
-                  searchButton.setTitleColor(clear, for: .normal)
-                  searchBar.textColor = clear
-              case "Clouds":
-                  weatherImageView.image = UIImage(named: "Cloudy")
-                  background.backgroundColor = cloud
-                  backgroundtwo.backgroundColor = cloud
-                  scroll.backgroundColor = cloud
-                  searchButton.setTitleColor(cloud, for: .normal)
-                  searchBar.textColor = cloud
-              case "Snow":
-                  weatherImageView.image = UIImage(named: "Snow")
-                  background.backgroundColor = snow
-                  backgroundtwo.backgroundColor = snow
-                  scroll.backgroundColor = snow
-                  searchButton.setTitleColor(snow, for: .normal)
-                  searchBar.textColor = snow
-              case "Atmosphere":
-                  weatherImageView.image = UIImage(named: "Fog")
-                  background.backgroundColor = fog
-                  backgroundtwo.backgroundColor = fog
-                  scroll.backgroundColor = fog
-                  searchButton.setTitleColor(fog, for: .normal)
-                  searchBar.textColor = fog
-              case "Rain":
-                  weatherImageView.image = UIImage(named: "Rain")
-                  background.backgroundColor = rain
-                  backgroundtwo.backgroundColor = rain
-                  scroll.backgroundColor = rain
-                  searchButton.setTitleColor(rain, for: .normal)
-                  searchBar.textColor = rain
-              case "Drizzle":
-                  weatherImageView.image = UIImage(named: "Rain")
-                  background.backgroundColor = rain
-                  backgroundtwo.backgroundColor = rain
-                  scroll.backgroundColor = rain
-                  searchButton.setTitleColor(rain, for: .normal)
-                  searchBar.textColor = rain
-              case "Thunderstorm":
-                  weatherImageView.image = UIImage(named: "Thunder")
-                  background.backgroundColor = thunder
-                  backgroundtwo.backgroundColor = thunder
-                  scroll.backgroundColor = thunder
-                  searchButton.setTitleColor(thunder, for: .normal)
-                  searchBar.textColor = thunder
-              default:
-                  weatherImageView.image = UIImage(named: "Cloudy")
-                  background.backgroundColor = rain
-                  backgroundtwo.backgroundColor = rain
-                  scroll.backgroundColor = rain
-                  searchButton.setTitleColor(rain, for: .normal)
-                  searchBar.textColor = rain
-              }
+    func errormeesage (){
+        
+        errormsgLabel.text = "Current conditions not found"
+        weatherImageView.image = nil
+        TempLabel.text = ""
+        HumidityLabel.text = ""
+        PrecipitationLabel.text = ""
+        PressureLabel.text = ""
+        cloudcoverLabel.text = ""
+        windLabel.text = ""
+        return
+        
+    }
+    func setWeather(  tempF: NSArray, tempC: NSArray ,humidity:NSArray,windSpeed:NSArray,windDirection:NSArray, precipInches:NSArray, pressureInches:NSArray,cloudcover:NSArray, weatherIconUrl:NSArray,winddirDegree:NSArray,windspeedKmph:NSArray) {
+
+    TempLabel.text = "\(tempC.map({ "\($0)" }).joined(separator: ", "))°C/\(tempF.map({ "\($0)" }).joined(separator: ", "))°F"
+        HumidityLabel.text = "\(humidity.map({ "\($0)" }).joined(separator: ", "))%"
+        PrecipitationLabel.text = "\(precipInches.map({ "\($0)" }).joined(separator: ", "))mm"
+        PressureLabel.text = "\(pressureInches.map({ "\($0)" }).joined(separator: ", "))mbar"
+        cloudcoverLabel.text = "\(cloudcover.map({ "\($0)" }).joined(separator: ", "))%"
+        
+        windLabel.text = "\(windspeedKmph.map({ "\($0)" }).joined(separator: ", "))kmph/\(windSpeed.map({ "\($0)" }).joined(separator: ", "))mph \(windDirection.map({ "\($0)" }).joined(separator: ", "))(\(winddirDegree.map({ "\($0)" }).joined(separator: ", "))°)"
+        windLabel.sizeToFit()
+        errormsgLabel.text = ""
+        
+  
+        let weatherIcon =  weatherIconUrl.map({ "\($0)" }).joined(separator: ", ")
+        print(weatherIcon)
+        let trimmed = String(weatherIcon.filter { !" \n\t\r".contains($0) })
+        let trimmed1 = trimmed.replacingOccurrences(of: "\"", with: "")
+        let trimmed2 = trimmed1.replacingOccurrences(of: "\\(", with: "", options: .regularExpression)
+        let trimmed3 = trimmed2.replacingOccurrences(of: "\\)", with: "", options: .regularExpression)
+      print(trimmed2)
+     
+        let weatherUrl: String = trimmed3
+        let imageUrl = URL(string: weatherUrl)
+     
+        let data = try? Data(contentsOf: imageUrl!)
+        let image = UIImage(data:data!)
+        weatherImageView.image = image
+        
+              
+              
           }
       }
       //MARK: FUNCTIONS
-      extension String {
-          func capitalizingFirstLetter() -> String {
-              return prefix(1).uppercased() + self.lowercased().dropFirst()
-          }
-      }
-      func compassDirection(heading: Double) -> String {
-          if heading < 0 { return "" }
-          let directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
-          let index = Int((heading + 22.5) / 45.0) & 7
-          return directions[index]
-      }
+     
+        
 
 
 
-}
+
+
    
+
